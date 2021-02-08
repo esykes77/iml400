582a981d56a23a6ee94bdb9f52654ec9bcad6c9b
esykes77 <60246847+esykes77@users.noreply.github.com>
2021-02-08 15:12:39 -0800

    Added fruits

--- cybernetic-garden/cybergarden.js
+++ cybernetic-garden/cybergarden.js
@@ -3,6 +3,17 @@ console.log("Hello World");
 $(document).ready(function(){
 
 
-$(".baby").click(function(){
-  alert("Hello!");
-});
+
+  $(".baby").click(function(){
+    alert("I like to think and the sooner the better of a cybernetic meadow where mammals and computers live together in mutually programming harmony like pure water touching pure sky");
+  });
+
+  $(".venus").click(function(){
+    alert("I like to think, right now please, of a cybernetic forest filled with pines and electronics where deer stroll peacefully past computers as if they were flowers with spinning blossoms.");
+  });
+
+  $(".deer").click(function(){
+    alert("I like to think, it has to be, of a cybernetic ecology where we are free of our labors and joined back to nature, returned to our mammal brothers and sisters, and all watched over by machines of loving grace. -Richard Brautigan");
+  });
+
+  });
